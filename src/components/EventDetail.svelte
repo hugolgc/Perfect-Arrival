e79724b71c0060952c2757cb17998320f648a14a
hugolgc <lagache.hugo@hotmail.fr>
2023-10-18 12:02:52 +0200

    🐞 Fix information component

--- src/components/EventDetail.svelte
+++ src/components/EventDetail.svelte
@@ -11,11 +11,26 @@
 </script>
 
 <section>
-  <button on:click={deleteEvent}>Supprimer</button>
+  <button on:click={deleteEvent}>
+    <svg
+      width="64"
+      height="64"
+      viewBox="0 0 64 64"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M32 0C14.3262 0 0 14.3262 0 32C0 49.6738 14.3262 64 32 64C49.6738 64 64 49.6738 64 32C64 14.3262 49.6738 0 32 0ZM41.8462 34.4615C42.499 34.4615 43.1251 34.2022 43.5867 33.7406C44.0484 33.2789 44.3077 32.6528 44.3077 32C44.3077 31.3472 44.0484 30.7211 43.5867 30.2594C43.1251 29.7978 42.499 29.5385 41.8462 29.5385H22.1538C21.501 29.5385 20.8749 29.7978 20.4133 30.2594C19.9516 30.7211 19.6923 31.3472 19.6923 32C19.6923 32.6528 19.9516 33.2789 20.4133 33.7406C20.8749 34.2022 21.501 34.4615 22.1538 34.4615H41.8462Z"
+        fill="#8D8D8D"
+      />
+    </svg>
+  </button>
   <input
     bind:value={event.name}
     type="text"
-    placeholder="Évènement"
+    placeholder="Événement"
     class="text"
   />
   <input bind:value={event.time} type="time" class="time" />
@@ -30,6 +45,7 @@
 
   .text {
     flex: 1 1 0%;
+    text-align: center;
   }
 
   .time {
--- src/components/Information.svelte
+++ src/components/Information.svelte
@@ -22,7 +22,7 @@
   }
 
   function updateTimes(isArrival: boolean) {
-    const MINUTE_TO_MILLISECONDS = 1000 * 60;
+    const MINUTE_TO_MILLISECONDS = 60_000;
     const baseTime = isArrival ? arrivalTime : departureTime;
     const baseDate = new Date(2000, 8, 23);
 
@@ -44,7 +44,11 @@
       baseDate.getMinutes()
     )}`;
 
-    isArrival ? (departureTime = dateToTime) : (arrivalTime = dateToTime);
+    if (isArrival) {
+      departureTime = dateToTime;
+    } else {
+      arrivalTime = dateToTime;
+    }
   }
 
   function addZero(value: number) {
@@ -63,7 +67,20 @@
     />
   </section>
   <button on:click={createEvent}>
-    <!-- ... Votre icône ... -->
+    <svg
+      width="64"
+      height="64"
+      viewBox="0 0 64 64"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M32 64C40.4869 64 48.6263 60.6286 54.6274 54.6274C60.6286 48.6263 64 40.4869 64 32C64 23.5131 60.6286 15.3737 54.6274 9.37258C48.6263 3.37142 40.4869 0 32 0C23.5131 0 15.3737 3.37142 9.37258 9.37258C3.37142 15.3737 0 23.5131 0 32C0 40.4869 3.37142 48.6263 9.37258 54.6274C15.3737 60.6286 23.5131 64 32 64ZM35 19C35 18.2044 34.6839 17.4413 34.1213 16.8787C33.5587 16.3161 32.7957 16 32 16C31.2044 16 30.4413 16.3161 29.8787 16.8787C29.3161 17.4413 29 18.2044 29 19V29H19C18.2044 29 17.4413 29.3161 16.8787 29.8787C16.3161 30.4413 16 31.2044 16 32C16 32.7957 16.3161 33.5587 16.8787 34.1213C17.4413 34.6839 18.2044 35 19 35H29V45C29 45.7957 29.3161 46.5587 29.8787 47.1213C30.4413 47.6839 31.2044 48 32 48C32.7957 48 33.5587 47.6839 34.1213 47.1213C34.6839 46.5587 35 45.7957 35 45V35H45C45.7957 35 46.5587 34.6839 47.1213 34.1213C47.6839 33.5587 48 32.7957 48 32C48 31.2044 47.6839 30.4413 47.1213 29.8787C46.5587 29.3161 45.7957 29 45 29H35V19Z"
+        fill="#8D8D8D"
+      />
+    </svg>
   </button>
   <section>
     <p>Arrivée</p>
